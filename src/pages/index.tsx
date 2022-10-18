351d78e5165dc4a1bce67dad40fa6692cc3536de
AlexJamisone <business.homeit@gmail.com>
2022-10-18 18:30:50 +0300

    add prisma clien db /w mongo db and base auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Home: NextPage = () => {
+	const { data } = useSession()
+	console.log("Here is Data", data)
 	return (
 		<div>
-			Hello Home Page
+			{
+				data?.user ? <button onClick={() => signOut()}>Sign Out</button> : <button onClick={() => signIn('google')}>Sign In</button>
+					
+			}
+			
+			{data?.user?.name}
 		</div>
 	)
 }
