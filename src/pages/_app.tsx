a93aeb3ac6e5ac382334763f3dec5b3fa2b15dfa
AlexJamisone <business.homeit@gmail.com>
2022-10-18 20:06:19 +0300

    add base theme provide /w chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app'
+import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../chakra/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
 	return (
-		<SessionProvider>
-			<Component {...pageProps} />
+		<SessionProvider session={pageProps.session}>
+			<ChakraProvider theme={theme}>
+				<Component {...pageProps} />
+			</ChakraProvider>
 		</SessionProvider>
 	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,15 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Home: NextPage = () => {
 	const { data } = useSession()
-	console.log("Here is Data", data)
+	console.log('Here is Data', data)
 	return (
 		<div>
-			{
-				data?.user ? <button onClick={() => signOut()}>Sign Out</button> : <button onClick={() => signIn('google')}>Sign In</button>
-					
-			}
-			
+			{data?.user ? (
+				<button onClick={() => signOut()}>Sign Out</button>
+			) : (
+				<button onClick={() => signIn('google')}>Sign In</button>
+			)}
+
 			{data?.user?.name}
 		</div>
 	)
