1990b91a91603f181713747751ded9564fe2f2d1
AlexJamisone <business.homeit@gmail.com>
2022-10-26 16:23:55 +0300

    Add Search component

--- src/components/Chat/Convirsation/Modal/Modal.tsx
+++ src/components/Chat/Convirsation/Modal/Modal.tsx
@@ -5,23 +5,47 @@ import {
 	ModalHeader,
 	ModalBody,
 	ModalCloseButton,
+	Stack,
+	Input,
+	Button,
 } from '@chakra-ui/react'
-import { FC } from 'react'
+import React, { FC, useState } from 'react'
 
 interface ModalProps {
 	isOpen: boolean
 	onClose: () => void
 }
 
-const ConversationModal: FC<ModalProps> = ({isOpen, onClose}) => {
+const ConversationModal: FC<ModalProps> = ({ isOpen, onClose }) => {
+	const [username, setUsername] = useState('')
+
+	const onSearch = async (e: React.FormEvent) => {
+		e.preventDefault()
+		console.log("Inside on submit", username)
+	}
 	return (
 		<>
 			<Modal isOpen={isOpen} onClose={onClose}>
 				<ModalOverlay />
-				<ModalContent>
+				<ModalContent bg="#2d2d2d">
 					<ModalHeader>Modal Title</ModalHeader>
 					<ModalCloseButton />
-					<ModalBody></ModalBody>
+					<ModalBody>
+						<form onSubmit={onSearch}>
+							<Stack spacing={4}>
+								<Input
+									placeholder="Enter a username"
+									value={username}
+									onChange={(e) =>
+										setUsername(e.target.value)
+									}
+								/>
+								<Button type='submit' disabled={!username}>
+									Search
+								</Button>
+							</Stack>
+						</form>
+					</ModalBody>
 				</ModalContent>
 			</Modal>
 		</>
