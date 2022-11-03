e2c35ec51c9142d6cd020467a1050b8bc95e49d6
AlexJamisone <business.homeit@gmail.com>
2022-11-03 23:04:31 +0300

    Start work on websocek apollo client

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -1,11 +1,38 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
+import { getMainDefinition } from '@apollo/client/utilities'
 
 const httpLink = new HttpLink({
 	uri: 'http://localhost:4000/graphql',
 	credentials: 'include',
 })
 
+const wsLink =
+	typeof window !== 'undefined'
+		? new GraphQLWsLink(
+				createClient({
+					url: 'ws://localhost:4000/graphql/subscriptions',
+				})
+		  )
+		: null
+
+const link =
+	typeof window !== 'undefined' && wsLink != null
+		? split(
+				({ query }) => {
+					const def = getMainDefinition(query)
+					return (
+						def.kind === 'OperationDefinition' &&
+						def.operation === 'subscription'
+					)
+				},
+				wsLink,
+				httpLink
+		  )
+		: httpLink
+	
 export const client = new ApolloClient({
-	link: httpLink,
+	link,
 	cache: new InMemoryCache(),
 })
